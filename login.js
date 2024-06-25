9ba86c4be15d86b910faec7a091939b8d0a28040
Joseph Isaac Asencio Ortiz <josephasencio64@gmail.com>
2024-06-25 16:41:02 -0600

    new6

--- login.js
+++ login.js
@@ -1,16 +1,16 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    const username = document.getElementById('username').value;
+    const role = document.getElementById('role').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('error-message');
-    const successMessage = document.getElementById('success-message');
     
-    if (username === 'admin' && password === 'admin123') {
-        successMessage.style.display = 'block';
-        errorMessage.style.display = 'none';
+    if ((role === 'administracion' && password === '0') || (role === 'encargado' && password === '1')) {
+        if (role === 'administracion') {
+            window.location.href = 'CuadreAdmi.html';
+        } else if (role === 'encargado') {
+            window.location.href = 'CuadreEncargado.html';
+        }
     } else {
         errorMessage.style.display = 'block';
-        successMessage.style.display = 'none';
     }
 });
